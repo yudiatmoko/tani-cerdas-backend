0e548f02a06ec3a3748699f88fb93fe6b23d8aed
DiazDewasaPutra <aarlo1277@gmail.com>
2024-11-25 09:17:19 +0700

    test

--- backend/routes/akunRoute.js
+++ backend/routes/akunRoute.js
@@ -4,17 +4,14 @@ import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// Route yang dapat diakses oleh semua pengguna
-router.get("/akun", akunModelController.getAllAkun); // Mendapatkan semua akun
-router.get("/akun/:id", akunModelController.getAkunById); // Mendapatkan akun berdasarkan ID
-router.get("/akun/by-name", akunModelController.getAkunByName); // Mendapatkan akun berdasarkan nama
-
-// Route yang hanya bisa diakses oleh user dan pakar
+router.get("/akun", akunModelController.getAllAkun);
+router.get("/akun/:id", akunModelController.getAkunById);
+router.get("/akun/by-name", akunModelController.getAkunByName);
 router.put(
     "/akun/:id",
-    authMiddleware.authenticate, // Harus login
-    authMiddleware.authorizeRoles(1, 2), // Hanya untuk user (1) dan pakar (2)
-    akunModelController.updateAkunFields // Memperbarui data akun
+    authMiddleware.authenticate, 
+    authMiddleware.authorizeRoles(1, 2),
+    akunModelController.updateAkunFields 
 );
 
 export default router;
