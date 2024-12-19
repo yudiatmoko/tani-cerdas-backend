8711a4ffafd275374db258a10b90f06ea5cc9ad9
yudiatmoko <yudiatmokoilham@gmail.com>
2024-12-19 22:13:19 +0700

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,21 @@ import dotenv from "dotenv";
 import { testConnection } from "./database/db.js";
 import bodyParser from "body-parser";
 import router from "./routes/index.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use("/api/v1", router);
 
